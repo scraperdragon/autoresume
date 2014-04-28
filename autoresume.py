843369493bfadcb65b8a01840472b9823cde3a85
Dragon Dave <dragon@scraperwiki.com>
2014-04-28 15:56:22 +0100

    saves state

--- autoresume.py
+++ autoresume.py
@@ -1,3 +1,21 @@
 class AutoResume(object):
     def __init__(self, name='page'):
         self.name = name
+        self.backend = ScraperwikiBackend()
+
+    def save_state(self, value):
+        self.backend.save(self.name, value)
+
+    @property
+    def state(self):
+        return self.backend.state(self.name)
+
+
+class ScraperwikiBackend(object):
+    def save(self, name, value):
+        import scraperwiki
+        scraperwiki.sql.save_var(name, value)
+
+    def state(self, name):
+        import scraperwiki
+        return scraperwiki.sql.get_var(name)
--- tests.py
+++ tests.py
@@ -4,5 +4,13 @@ def test_exists():
     autoresume.AutoResume()
 
 def test_has_a_name():
-    autoresume.AutoResume().name = 'page'
-    autoresume.AutoResume(name='name').name = 'name'
+    assert autoresume.AutoResume().name == 'page'
+    assert autoresume.AutoResume(name='name').name == 'name'
+
+def test_saves_to_db():
+    unnamed = autoresume.AutoResume()
+    named = autoresume.AutoResume(name='named')
+    unnamed.save_state(4)
+    named.save_state(5)
+    assert unnamed.state == 4
+    assert named.state == 5

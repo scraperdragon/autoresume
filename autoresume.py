ef9c7c58025aa6c25a10f807e653fd5421444f58
Dragon Dave <dragon@scraperwiki.com>
2014-05-30 10:46:00 +0100

    docstrings

--- autoresume.py
+++ autoresume.py
@@ -2,6 +2,7 @@ import scraperwiki
 
 
 class AutoResumeError(Exception):
+    "Generic error from autoresume"
     pass
 
 
@@ -16,16 +17,22 @@ class AutoResume(object):
         self.backend = ScraperwikiBackend()
 
     def save_state(self, value):
+        "We record that we have completed 'value'"
         self.backend.save(self.name, value)
 
     def reset(self):
+        "Restart next time"
         self.backend.save(self.name, None)
 
     @property
     def state(self):
+        "What did we last complete?"
         return self.backend.state(self.name)
 
     def iter(self, iterator):
+        "We assume the iterator is unchanged except
+         for appending: i.e. order is unchanged and
+         old value must be present."
         prev_item = None
         sentinel = self.state
         found_sentinel = sentinel is None
@@ -48,6 +55,7 @@ class AutoResume(object):
             raise NoSentinelError(sentinel)
 
     def magic(self, iterator, template="{}"):
+        "Autoformat string with iterator."
         for item in self.iter(iterator):
             yield template.format(item)
 
